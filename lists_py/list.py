3630f39b55f296d4cc287387d663dfb461db3824
Tinoet <88379020+Tinoet@users.noreply.github.com>
2022-03-26 21:30:02 +0100

    Add files via upload

---
+++ lists_py/list.py
@@ -0,0 +1,5 @@
+names =['elijah', 'tochi', 'eze', 'edith']
+# to set new elements to items in ist
+names[0] = 'eljia'
+print(names[0])
+# we use te index to call up individual strings, we can also make use of the : to call up from certain number to the last
